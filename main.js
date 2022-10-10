8e66cd2c9e6058b7d3b34fd692b05de954de41fa
Ziabeher Phillips <zabq02@gmail.com>
2022-10-10 16:39:28 -0400

    more sections

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Scene
 const scene = new THREE.Scene();
+// const textureLoader = new THREE.TextureLoader();
+// scene.background = textureLoader.load('./green.png')
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
 camera.position.setZ(5)
 const renderer = new THREE.WebGLRenderer({
@@ -14,10 +16,25 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const ambientLight = new THREE.AmbientLight();
 scene.add(ambientLight)
 
+
 // Axes helper
 // const axesHelper = new THREE.AxesHelper(10, 10, 10);
 // axesHelper.setColors('white', 'blue', 'red')
 // scene.add(axesHelper);
+
+
+
+// Texture Loader
+// import bg from './bg.jpg'
+// const cubeTextureLoader = new THREE.CubeTextureLoader();
+// scene.background = cubeTextureLoader.load([
+//   bg,
+//   bg,
+//   bg,
+//   bg,
+//   bg,
+//   bg,
+// ])
 
 // Orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -33,9 +50,11 @@ loader.load('./3D-renders/space3d/scene.gltf', (gltf) => {
 
 // Laptop GLTF 3D model
 let laptop;
-loader.load('./3D-renders/laptop/scene.gltf', (gltf) => {
-  gltf.scene.scale.set(.5, .5, .5);
-  gltf.scene.position.set(1, -2, 3)
+loader.load('./3D-renders/laptop2/scene.gltf', (gltf) => {
+  laptop = gltf.scene
+  gltf.scene.scale.set(.6, .6, .6);
+  gltf.scene.position.set(3, -1, 3)
+  gltf.scene.rotateY(4.6)
   scene.add(gltf.scene);
 })
 
