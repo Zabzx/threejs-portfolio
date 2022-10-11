bbdf41a70fa2463f3a6c31ef9c85c562a397b9e4
Ziabeher Phillips <zabq02@gmail.com>
2022-10-11 13:27:09 -0400

    contact section

--- main.js
+++ main.js
@@ -56,7 +56,22 @@ loader.load('./3D-renders/laptop2/scene.gltf', (gltf) => {
   gltf.scene.position.set(3, -1, 3)
   gltf.scene.rotateY(4.6)
   scene.add(gltf.scene);
-})
+});
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x * 5, y, z * 5);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
 
 // Animate
 function animate() {
